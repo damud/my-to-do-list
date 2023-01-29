1c1f91df4aba25186e5f53960215e1f4356760aa
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-01-29 20:09:56 +0100

    Get list of task from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
 import ListHeader from "./components/ListHeader";
+import {useEffect, useState} from 'react';
 
 const App = () => {
+  const userEmail = 'dan@test.pl'
+  const [tasks, setTasks] = useState(null)
+  const getData = async () => {
+   
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const json = await response.json();
+      setTasks(json)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => getData, [])
+  console.log(tasks)
   return (
     <div className="app">
       <ListHeader listName={'🌴 Holiday tick list'} />
