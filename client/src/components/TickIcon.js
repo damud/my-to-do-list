7c36af26d518f940d8108492405cd777698c4371
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-01-30 19:55:56 +0100

    Create TickIcon.js component

--- client/src/components/TickIcon.js
+++ client/src/components/TickIcon.js
@@ -1,8 +1,14 @@
 const TickIcon = () => {
   return (
-    <div>
-      
-    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" 
+    className="tick"
+    width="25" 
+    height="25" 
+    viewBox="0 0 512 512">
+      <path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192s192-86 192-192Z"/>
+      <path d="M352 176L217.6 336L160 272"/>
+      </svg>
+
   )
 }
 
