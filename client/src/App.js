0313f956f41d66b2389831eaa70fa2142201b8d4
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-01-30 19:35:54 +0100

    Create ListItem.js components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import ListHeader from "./components/ListHeader";
-import {useEffect, useState} from 'react';
+import ListItem from "./components/ListItem";
+import {useEffect, useState} from "react";
 
 const App = () => {
-  const userEmail = 'dan@test.pl'
+  const userEmail = "dan@test.pl"
   const [tasks, setTasks] = useState(null)
   const getData = async () => {
    
@@ -17,10 +18,14 @@ const App = () => {
 
   useEffect(() => getData, [])
   console.log(tasks)
+
+  // Sort by date
+  const sortedTasks =tasks?.sort((a,b) => new Date(a.date) - new Date(b.date));
+
   return (
     <div className="app">
-      <ListHeader listName={'🌴 Holiday tick list'} />
-      
+      <ListHeader listName={"🌴 Holiday tick list"} />
+      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} />)}
     </div>
   );
 }
