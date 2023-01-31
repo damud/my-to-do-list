769f5b8b4c27d91379b2d797c757ae81417331d1
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-01-31 19:14:16 +0100

    Create Modal.js component

--- client/src/components/ListHeader.js
+++ client/src/components/ListHeader.js
@@ -1,3 +1,5 @@
+import Modal from "./Modal";
+
 const ListHeader = ({listName}) => {
 
     const signOut = () => {
@@ -12,6 +14,7 @@ const ListHeader = ({listName}) => {
       <button className="create">ADD NEW</button>
       <button className="signout" onClick={signOut}>SIGN OUT</button>
         </div>    
+        <Modal />
       </div>
   )
 }
