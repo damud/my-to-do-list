f51779d3b571c4cf3fae6404f9e50e75e0bb2391
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-01-31 19:39:41 +0100

    Add and close modal functions

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,8 +1,20 @@
-const Modal = () => {
-  
-  const mode = "edit";
-const handleChange = () => {
-  console.log('changing!')
+import {useState} from 'react';
+
+const Modal = ({mode, setShowModal}) => {
+  const editMode = mode === "edit" ? true :false;
+  const [data, setData] = useState({
+    user_email: "",
+    title: "",
+    progress: "",
+    date: editMode ? "" : new Date()
+  })
+const handleChange = (e) => {
+  const {name, value} = e.target;
+
+  setData(data => ({
+    ...data,
+    [name] : value
+  }))
 }
 
   return (
@@ -10,7 +22,7 @@ const handleChange = () => {
       <div className="modal">
         <div className="form-title-container">
           <h3>Let's {mode} your task</h3>
-          <button>X</button>
+          <button onClick={() => setShowModal(false)}>X</button>
         </div>
 <form>
   <input
@@ -18,7 +30,7 @@ const handleChange = () => {
   maxLenght={30}
   placeholder="Your task goes here"
   name="title"
-  value={""}
+  value={data.title}
   onChange={handleChange} 
   />
   <br />
@@ -30,7 +42,7 @@ const handleChange = () => {
   min="0"
   max="100"
   name="progress"
-  value={""}
+  value={data.progress}
   onChange={handleChange}
   />
   <input className={mode} type="submit"/>
