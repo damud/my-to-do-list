21e7ecf30b083b778a18a35b80bbd634628772b8
Daniel Mudlaff <dmudlaff89@gmail.com>
2023-02-02 18:20:49 +0100

    Make handleSubmit function on client side

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -2,12 +2,24 @@ import { useState } from "react";
 
 const Auth = () => {
   const [isLogIn, setIsLogin] = useState(true);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
 
   const viewLogin = (status) => {
    
     setError(null);
     setIsLogin(status);
+  }
+
+  const handleSubmit = async (e, endpoint) => {
+    e.preventDefault();
+    if (!isLogIn && password !== confirmPassword) {
+      setError("Make sure password match!");
+      return
+    }
+    await fetch(`${process.env.REACT_APP_SERVERURL}/${endpoint}`)
   }
   
   return (
@@ -18,7 +30,7 @@ const Auth = () => {
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
         {!isLogIn && <input type="password" placeholder="confirm password" />}
-        <input type="submit" className="create" />
+        <input type="submit" className="create" onClick={(e) => handleSubmit(e, isLogIn ? "login" : "signup")} />
         {error && <p>{error}</p>}
       </form>
       <div className="auth-options">
